668b6d66285f11b92a8c87b23917a688d88e7d49
hirofumi.kinoshita <knfm1245_4344@yahoo.co.jp>
2020-04-01 11:14:02 +0000

    syuusei strong parameter

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,10 +46,11 @@ class TasksController < ApplicationController
         flash[:seccess] = 'Task は正常に削除されました'
         redirect_to tasks_url
     end
-end
-
+    
 private
 
 def task_params
     params.require(:task).permit(:content)
+end    
+    
 end
