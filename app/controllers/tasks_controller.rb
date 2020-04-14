b4562716fe5d1d7b749954c06fbea752136311b8
hirofumi.kinoshita <knfm1245_4344@yahoo.co.jp>
2020-04-14 13:29:59 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
 before_action :set_task, only: [:show, :edit, :update, :destroy]
+before_action :require_user_logged_in
+before_action :correct_user, only: [:destroy]
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
     
     def show
@@ -12,7 +14,7 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
     end
     
     def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
         flash[:success] = 'Task が正常に設定されました'
@@ -52,6 +54,13 @@ end
 
 def task_params
     params.require(:task).permit(:content, :status)
-end    
+end
+
+def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/create'
-  get 'users/new'
+ #get 'sessions/new'
+ #get 'sessions/create'
+ #get 'sessions/destroy'
+ #get 'users/create'
+ #get 'users/new'
   #get 'toppages/index'
     #root to: 'toppages#index'
     root to: 'tasks#index'
     
     get 'signup', to: 'users#new'
     get 'user', to: 'tasks#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
     resources :tasks
     resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
